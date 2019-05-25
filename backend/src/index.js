0441543736ba6672d3ff2908dc274fd3999ee6e9
github username <matthewdizon94@protonmail.com>
2019-05-25 16:29:30 -0400

    added all dependancies to backend: express,mongoose, nodemon

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const port = process.env.PORT || 3000
+const db = mongoose.connection
+
+mongoose.connect('mongodb://localhost/databasename',{useNewUrlParser:true})
+app.use(express.json())
+
+db.on('error',(error) => console.error(error))
+db.once(`open`, () => console.log(`Connected to DB`))
+
+app.listen(port, ()=> console.log(`Listen on ${port}`))
